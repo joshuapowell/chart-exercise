6512ad94e2b0f3e7a355f7432771ab341db93ee6
Joshua Powell <joshua@viable.io>
2019-02-22 17:30:35 -0500

    Added additional tests

--- tests/test_chart.py
+++ tests/test_chart.py
@@ -132,92 +132,113 @@ class TestExample(unittest.TestCase):
 
     def test_chartIsNumberWholeNumber(self):
         """Whole Number."""
-        value1 = Chart(file="", show_values=False).is_number(1)
-        self.assertTrue(value1)
+        value = Chart(file="", show_values=False).is_number(1)
+        self.assertTrue(value)
 
     def test_chartIsNumberWholeNumberAsString(self):
         """Whole Number as String."""
-        value2 = Chart(file="", show_values=False).is_number('1')
-        self.assertTrue(value2)
+        value = Chart(file="", show_values=False).is_number('1')
+        self.assertTrue(value)
         
     def test_chartIsNumberFloat(self):
         """Float."""
-        value3 = Chart(file="", show_values=False).is_number(1.19283)
-        self.assertTrue(value3)
+        value = Chart(file="", show_values=False).is_number(1.19283)
+        self.assertTrue(value)
 
     def test_chartIsNumberFloatAsString(self):
         """Float as String."""
-        value3 = Chart(file="", show_values=False).is_number('1.19283')
-        self.assertTrue(value3)
+        value = Chart(file="", show_values=False).is_number('1.19283')
+        self.assertTrue(value)
 
     def test_chartIsNumberNegativeFloat(self):
         """Negative Float."""
-        value4 = Chart(file="", show_values=False).is_number(-1.19283)
-        self.assertTrue(value4)
+        value = Chart(file="", show_values=False).is_number(-1.19283)
+        self.assertTrue(value)
 
     def test_chartIsNumberNegativeFloatAsString(self):
         """Negative Float as String."""
-        value4 = Chart(file="", show_values=False).is_number('-1.19283')
-        self.assertTrue(value4)
+        value = Chart(file="", show_values=False).is_number('-1.19283')
+        self.assertTrue(value)
 
     def test_chartIsNumberNegativeWholeNumber(self):
         """Negative Whole Number."""
-        value5 = Chart(file="", show_values=False).is_number(-1)
-        self.assertTrue(value5)
+        value = Chart(file="", show_values=False).is_number(-1)
+        self.assertTrue(value)
 
     def test_chartIsNumberNegativeWholeNumberAsString(self):
         """Negative Whole Number as String."""
-        value5 = Chart(file="", show_values=False).is_number('-1')
-        self.assertTrue(value5)
+        value = Chart(file="", show_values=False).is_number('-1')
+        self.assertTrue(value)
 
     def test_chartIsNumberNonNumberValueAsString(self):
         """Non-numeric String Value."""
-        value6 = Chart(file="", show_values=False).is_number('a')
-        self.assertFalse(value6)
+        value = Chart(file="", show_values=False).is_number('a')
+        self.assertFalse(value)
 
 
     def test_chartIsNumberWholeNumberGetValue(self):
         """Whole Number."""
-        value1 = Chart(file="", show_values=False).get_value(1)
-        self.assertTrue(value1)
+        value = Chart(file="", show_values=False).get_value(1)
+        self.assertTrue(value)
 
     def test_chartIsNumberWholeNumberAsStringGetValue(self):
         """Whole Number as String."""
-        value2 = Chart(file="", show_values=False).get_value('1')
-        self.assertTrue(value2)
+        value = Chart(file="", show_values=False).get_value('1')
+        self.assertTrue(value)
         
     def test_chartIsNumberFloatGetValue(self):
         """Float."""
-        value3 = Chart(file="", show_values=False).get_value(1.19283)
-        self.assertTrue(value3)
+        value = Chart(file="", show_values=False).get_value(1.19283)
+        self.assertTrue(value)
 
     def test_chartIsNumberFloatAsStringGetValue(self):
         """Float as String."""
-        value3 = Chart(file="", show_values=False).get_value('1.19283')
-        self.assertTrue(value3)
+        value = Chart(file="", show_values=False).get_value('1.19283')
+        self.assertTrue(value)
 
     def test_chartIsNumberNegativeFloatGetValue(self):
         """Negative Float."""
-        value4 = Chart(file="", show_values=False).get_value(-1.19283)
-        self.assertTrue(value4)
+        value = Chart(file="", show_values=False).get_value(-1.19283)
+        self.assertTrue(value)
 
     def test_chartIsNumberNegativeFloatAsStringGetValue(self):
         """Negative Float as String."""
-        value4 = Chart(file="", show_values=False).get_value('-1.19283')
-        self.assertTrue(value4)
+        value = Chart(file="", show_values=False).get_value('-1.19283')
+        self.assertTrue(value)
 
     def test_chartIsNumberNegativeWholeNumberGetValue(self):
         """Negative Whole Number."""
-        value5 = Chart(file="", show_values=False).get_value(-1)
-        self.assertTrue(value5)
+        value = Chart(file="", show_values=False).get_value(-1)
+        self.assertTrue(value)
 
     def test_chartIsNumberNegativeWholeNumberAsStringGetValue(self):
         """Negative Whole Number as String."""
-        value5 = Chart(file="", show_values=False).get_value('-1')
-        self.assertTrue(value5)
+        value = Chart(file="", show_values=False).get_value('-1')
+        self.assertTrue(value)
 
     def test_chartIsNumberNonNumberValueAsStringGetValue(self):
         """Non-numeric String Value."""
-        value6 = Chart(file="", show_values=False).get_value('a')
-        self.assertFalse(value6)
+        value = Chart(file="", show_values=False).get_value('a')
+        self.assertFalse(value)
 
+
+    def test_chartGetStyledValueWholeNumber(self):
+        """Whole Number."""
+        value = Chart(file="", show_values=False).get_styled_value(1, 1, 0, 0)
+        self.assertEquals(value, "|░ ")
+
+    def test_chartGetStyledValueWholeNumberFalse(self):
+        """Whole Number."""
+        value = Chart(file="", show_values=False).get_styled_value(1, 1, 0, 0)
+        self.assertNotEquals(value, " ░|")
+
+
+    def test_chartLegend(self):
+        """Whole Number."""
+        value = Chart(file="", show_values=False).get_legend(["year","profit","employees"])
+        self.assertEquals(value, "\nKey: profit ░ employees ▓ zero |")
+
+    def test_chartLegendAlternative(self):
+        """Whole Number."""
+        value = Chart(file="", show_values=False).get_legend(["profit","employees","year"])
+        self.assertEquals(value, "\nKey: employees ░ year ▓ zero |")
